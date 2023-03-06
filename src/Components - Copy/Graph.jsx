89b4ea4623ad3b2ffbcd72f6451628ea4c74b491
shvmsh20 <itsshivamsharma21@gmail.com>
2023-03-06 10:54:04 +0530

    add: sliders

--- src/Components - Copy/Graph.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Graph() {
-  return (
-    <div className='graph'>SIPGraph</div>
-  )
-}
-
-export default Graph;
--- src/Components - Copy/SideBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../Media/CSS/App.css';
-
-export default function SideBar() {
-  return (
-    <div className='side-bar'></div>
-  )
-}
