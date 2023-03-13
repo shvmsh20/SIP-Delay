0ad212f1b16d0e18f9be845eacee085214a3ac91
shvmsh20 <itsshivamsharma21@gmail.com>
2023-03-13 22:11:14 +0530

    connect graph with sliders

--- src/Components/InputArea.jsx
+++ src/Components/InputArea.jsx
@@ -4,7 +4,7 @@ import SliderArea from './SliderArea';
 function InputArea(props) {
   return (
     <div class="input">
-      <SliderArea index={0} mn={50000} mx={1000000} value={props.mi} setValue={props.changeMi}/>
+      <SliderArea index={0} mn={500} mx={10000} value={props.mi} setValue={props.changeMi}/>
       <SliderArea index={1} mn={1} mx={30} value={props.ip} setValue={props.changeIp}/>
       <SliderArea index={2} mn={1} mx={30} value={props.ror} setValue={props.changeRor}/>
       <SliderArea index={3} mn={1} mx={120} value={props.delay} setValue={props.changeDelay}/>
